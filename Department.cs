20ce278e69070a5faf669acb83dc319a161d572f
Nilesh Dhawade <dhawade.nilesh007@gmail.com>
2022-03-16 17:27:33 +0530

    Add files via upload

---
+++ Department.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsystem_Properties_Set_Get
+{
+    internal class Department
+    {
+        //private int dId;
+        //private string dName;
+
+        //public int Did
+        //{
+        //    set { dId = value; }
+        //    get { return dId; }
+        //}
+        //public string Dname
+        //{
+        //    set { dName = value; }
+        //    get { return dName; }
+        //}
+
+        //or we can do like following as well 
+        public int Id { get; set; }
+        public string Dname { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsystem_Properties_Set_Get
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Department obj = new Department();
+            obj.Id = 111;
+            obj.Dname = "mechanical";
+            Console.WriteLine(obj.Id+" "+obj.Dname);
+        }
+    }
+}
